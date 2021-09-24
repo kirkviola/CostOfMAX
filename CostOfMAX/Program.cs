da7f392fc7b0d7e447b09f90381d57d639cecb93
Matthew Kirkendall <kirk.viola@gmail.com>
2021-09-24 09:57:36 -0400

    Add project files.

---
+++ CostOfMAX/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DailyCostOfMAX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We're calculating the cost of MAX with decimal points this time.
+
+            var DaysOfMax = 65;
+            var CostOfMax = 15500m;
+            var PricePerDay = CostOfMax / DaysOfMax;
+            Console.WriteLine($"The daily cost of MAX boot camp is {Math.Round(PricePerDay, 2)} dollars per day.");
+
+        }
+    }
+}
+
